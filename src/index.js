51d064031310266f1b39619a10158c1ea0760dd8
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-21 12:16:42 -0400

    Refactored app comp

--- src/index.js
+++ src/index.js
@@ -6,17 +6,12 @@ import store from './store/index';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-function render() {
+
     ReactDOM.render(
         <Provider store={ store }>
             <App/>
         </Provider>
         , document.getElementById('root'));
-
-}
-
-store.subscribe(render);
-render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
