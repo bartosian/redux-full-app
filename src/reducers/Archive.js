bea8ff8ab0d2a6a3f9ef3aeb7e920fab8c021208
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-21 10:42:34 -0400

    Created first action archive

--- src/reducers/Archive.js
+++ src/reducers/Archive.js
@@ -1,7 +1,15 @@
+import { STORY_ARCHIVE }  from '../constants/actionTypes';
 const INITIAL_STATE = [];
+
+function applyStoryArchive(state, action) {
+    return [...state, action.id];
+}
 
 function archiveReducer(state=INITIAL_STATE, action) {
     switch (action.type) {
+        case STORY_ARCHIVE: {
+            return applyStoryArchive(state, action);
+        }
         default: return state;
     }
 }
